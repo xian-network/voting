ebee2c29df3cb494b42a2c4e09e6b0886100ecea
duelingbenjos <getrektwithme1@gmail.com>
2025-02-12 16:31:55 +0000

    update vite config

--- voting-spa/vite.config.ts
+++ voting-spa/vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/contributor-voting/',
+  base: '/',
   server: {
     allowedHosts: []
   }
