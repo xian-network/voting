eb1c6f71a064342149635f6728108a9405c47059
duelingbenjos <getrektwithme1@gmail.com>
2025-02-12 16:40:45 +0000

    update social links

--- voting-spa/src/App.svelte
+++ voting-spa/src/App.svelte
@@ -37,10 +37,11 @@
 
   // Add social media links
   const socialLinks = [
-    { href: "https://t.me/XianDAOGroup", icon: "telegram", label: "Telegram" },
-    { href: "https://discord.gg/xiandao", icon: "discord", label: "Discord" },
-    { href: "https://medium.com/@xiandao", icon: "medium", label: "Medium" },
-    { href: "https://github.com/XianDAO", icon: "github", label: "GitHub" },
+    { href: "https://t.me/xian_network", icon: "telegram", label: "Telegram" },
+    { href: "https://discord.gg/AcjdvgHnaT", icon: "discord", label: "Discord" },
+    { href: "https://xiannetwork.medium.com/", icon: "medium", label: "Medium" },
+    { href: "https://github.com/xian-network", icon: "github", label: "GitHub" },
+    { href: "https://forum.xian.org", icon: "discourse", label: "Discourse" },
   ];
 </script>
 
@@ -132,6 +133,17 @@
                       fill-rule="evenodd"
                       d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                       clip-rule="evenodd"
+                    />
+                  </svg>
+                {:else if icon === "discourse"}
+                  <svg
+                    class="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M12.103 0C18.666 0 24 5.485 24 11.997c0 6.51-5.33 11.99-11.9 11.99L0 24V11.79C0 5.28 5.532 0 12.103 0zm.116 4.563c-2.593-.003-4.996 1.352-6.337 3.57-1.33 2.208-1.387 4.957-.148 7.22L4.4 19.61l4.794-1.074c2.745 1.225 5.965.676 8.136-1.39 2.17-2.054 2.86-5.228 1.737-7.997-1.135-2.778-3.84-4.59-6.84-4.585h-.008z"
                     />
                   </svg>
                 {/if}
